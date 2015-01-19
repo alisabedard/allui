d67ea22c4ee4f273312e9ac812dc9820a9fc2650
Jeffrey E. Bedard <jefbed@gmail.com>
2015-01-19 00:25:59 -0500

    Added compound text entry facility, CompoundEntry.  Outputs entered text alongside user-provided label when Submit button pressed.

--- allui.py
+++ allui.py
@@ -11,19 +11,19 @@ import argparse
 
 class GridManager():
 	def __init__(self, max=3):
-		self.x=0
-		self.y=0
+		self.row=0
+		self.column=0
 		self.__max=max
 	def set_max(self, max):
 		self.__max=max
 	def new_row(self):
-		self.y=0
-		self.x+=1
-	def add(self, target):
-		target.grid(row=self.x, column=self.y, sticky=N+S+E+W, 
-				padx=2, pady=2)
-		self.y+=1
-		if(self.y >= self.__max):
+		self.column=0
+		self.row+=1
+	def add(self, target, **kwargs):
+		target.grid(row=self.row, column=self.column, sticky=N+S+E+W, 
+				padx=2, pady=2, **kwargs)
+		self.column+=1
+		if(self.column >= self.__max):
 			self.new_row()
 
 _grid = GridManager()
@@ -45,16 +45,30 @@ class CommandButton(Button):
 		print("EXECUTING: " + self.__command)
 		subprocess.call(self.__command + "&", shell=True)
 
+class CompoundEntry(Frame):
+	def __init__(self, parent, prompt):
+		super().__init__(master=parent)
+		self.__prompt=prompt
+		label=Label(self, text=prompt)
+		label.grid(row=0, column=0, padx=2)
+		text = Entry(self)
+		text.grid(row=0, column=2, columnspan=2)
+		self.__text=text
+		button = CommandButton(self, text="Submit", command=self.__entry_submit)
+		button.grid(row=0, column=4, padx=4, pady=2)
+	def __entry_submit(self):
+		print(self.__prompt + ':' + self.__text.get())
+
 class WidgetAction(argparse.Action):
 	def __init__(self, parent, option_strings, dest, nargs=None, **kwargs):
 		self._parent=parent
 		super().__init__(option_strings, dest, **kwargs)
 	def __call__(self, parser, namespace, values, option_string):
 		if option_string == '-e':
-			#_grid.new_row()
-			widget=Entry(self._parent)
+			widget=CompoundEntry(self._parent, values)
 		elif option_string == '-l':
 			widget=Label(self._parent, text=values)
+			_grid.x+=1
 		elif option_string == '-r':
 			_grid.set_max(int(values))	
 			_grid.new_row()
@@ -77,8 +91,6 @@ class AllUI(Frame):
 		button_help+="Put the button label definition after a '#' comment character."
 		parser.add_argument('-b', '--button', nargs=1, action=WidgetAction, 
 				parent=self, metavar="BUTTON", help=button_help)
-#		parser.add_argument('BUTTON', nargs='?', action=WidgetAction, parent=self,
-#				metavar="BUTTON", help=button_help)
 		parser.add_argument('-e', action=WidgetAction, parent=self, metavar='NAME')
 		parser.add_argument('-r', action=WidgetAction, parent=self, metavar='MAX')
 		args=parser.parse_args()
