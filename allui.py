7c258750e90ec4d7d8dfac0d3f8765da92c8c267
Jeffrey E. Bedard <jefbed@gmail.com>
2015-01-18 19:29:27 -0500

    Implemented generic argparse handler.  Implemented text entry and row max setting with row increment option.

--- allui.py
+++ allui.py
@@ -31,9 +31,9 @@ _grid = GridManager()
 
 class CommandButton(Button):
 	def __init__(self, master, text, command=None, command_string=None):
-		if(command==None):
+		if command is None:
 			command=self.__run_command
-		if(command_string==None):
+		if command_string is None:
 			command_string=text
 		self.__command=command_string
 		split_text=text.split('#', maxsplit=1)
@@ -41,25 +41,27 @@ class CommandButton(Button):
 		if(text == "Quit"):
 			command=quit
 		super().__init__(master=master, text=text, command=command)
-		_grid.add(self)
 	def __run_command(self):
 		print("EXECUTING: " + self.__command)
 		subprocess.call(self.__command + "&", shell=True)
 
-class ButtonAction(argparse.Action):
-	def __init__(self, parent, option_strings, dest, nargs=None, **kwargs):
-		self.__parent=parent
-		super().__init__(option_strings, dest, **kwargs)
-	def __call__(self, parser, namespace, values, option_string):
-		CommandButton(self.__parent, values)
-
-class LabelAction(argparse.Action):
+class WidgetAction(argparse.Action):
 	def __init__(self, parent, option_strings, dest, nargs=None, **kwargs):
 		self._parent=parent
 		super().__init__(option_strings, dest, **kwargs)
 	def __call__(self, parser, namespace, values, option_string):
-		label=Label(self._parent, text=values)
-		_grid.add(label)
+		if option_string == '-e':
+			#_grid.new_row()
+			widget=Entry(self._parent)
+		elif option_string == '-l':
+			widget=Label(self._parent, text=values)
+		elif option_string == '-r':
+			_grid.set_max(int(values))	
+			_grid.new_row()
+			return
+		else:
+			widget=CommandButton(self._parent, values)
+		_grid.add(widget)
 
 class AllUI(Frame):
 	def __init__(self, master=None, title="AllUI"):
@@ -69,12 +71,16 @@ class AllUI(Frame):
 		self.grid(sticky=N+S+E+W)
 	def parse_args(self):
 		parser=argparse.ArgumentParser(description="Generate a GUI")
-		parser.add_argument("-l", nargs=1, action=LabelAction, parent=self,
+		parser.add_argument('-l', '--label',nargs=1, action=WidgetAction, parent=self,
 				metavar="LABEL")
-		parser.add_argument("-b", nargs=1, action=ButtonAction, parent=self,
-				metavar="BUTTON")
-		parser.add_argument("BUTTON", nargs='*', action=ButtonAction, parent=self,
-				metavar="BUTTON")
+		button_help="Define a button which executes the specified shell command. "
+		button_help+="Put the button label definition after a '#' comment character."
+		parser.add_argument('-b', '--button', nargs=1, action=WidgetAction, 
+				parent=self, metavar="BUTTON", help=button_help)
+#		parser.add_argument('BUTTON', nargs='?', action=WidgetAction, parent=self,
+#				metavar="BUTTON", help=button_help)
+		parser.add_argument('-e', action=WidgetAction, parent=self, metavar='NAME')
+		parser.add_argument('-r', action=WidgetAction, parent=self, metavar='MAX')
 		args=parser.parse_args()
 
 
